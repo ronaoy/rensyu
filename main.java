373498a04fafad290729ec9e0e94695510284de3
ronaoy <rbfbcthfy6ragaxzggm8@icloud.com>
2024-10-21 13:32:59 +0900

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+package code01_03;
+
+public class main {
+
+	public static void main(String[] args) {
+		int age=20;
+		System.out.println("私の年齢は"+age);
+		age=31;
+		System.out.println("いや、本当の年齢は"+age);
+	}
+
+}
